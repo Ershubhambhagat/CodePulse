aaf12eed2ca448bcf62f9aa620aad6172802d6a7
Kumar <2090944@cognizant.com>
2023-11-06 13:05:59 +0530

    Till DB Create Done

---
+++ API/CodePulse.API/CodePulse.API/Model/Domain/BlogPost.cs
@@ -0,0 +1,21 @@
+﻿namespace CodePulse.API.Model.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Content { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle  { get; set; }
+        public DateTime PublishedDate { get; set; } 
+        public string Author { get; set; }
+        public string IsVisiblebool { get; set; }
+
+
+
+
+
+    }
+}
---
+++ API/CodePulse.API/CodePulse.API/Model/Domain/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CodePulse.API.Model.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+       
+    }
+}
